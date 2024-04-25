3c0e29954909710f5511484fdd3e06a278b13f98
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-25 11:35:01 -0300

    feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/compass/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.compass.course.repositories;
+
+import com.compass.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
