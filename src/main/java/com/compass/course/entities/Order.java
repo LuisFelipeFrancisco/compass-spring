0321ee7d1852bbf35b79aa2607e6fa40a5a7c7d8
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-25 13:17:59 -0300

    feat: OrderStatus enum

--- src/main/java/com/compass/course/entities/Order.java
+++ src/main/java/com/compass/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.compass.course.entities;
 
+import com.compass.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -13,20 +14,25 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // springboot2 dataformatter
     private Instant moment;
 
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +57,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
