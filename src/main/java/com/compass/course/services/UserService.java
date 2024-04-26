000191cf58b16c3dc9494aaea4c6797932476283
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-26 11:19:49 -0300

    feat: User insert, delete, update

--- src/main/java/com/compass/course/services/UserService.java
+++ src/main/java/com/compass/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.compass.course.services;
 
 import com.compass.course.entities.User;
 import com.compass.course.repositories.UserRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,26 @@ public class UserService {
     public User findById(Long id){
         Optional<User> obj = repo.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj){
+        return repo.save(obj);
+    }
+
+    public void delete(Long id){
+        repo.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repo.getReferenceById(id);
+        updateData(entity, obj);
+        return repo.save(entity);
+    }
+
+    private void updateData(@NotNull User entity, @NotNull User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 
 }
